93f5bcf9e6c233a7dde9b301028f9189c220901b
DBrodsky90 <DBrodsky90@gmail.com>
2014-11-19 15:10:11 -0500

    Attempting bing model

--- app/models/bing.rb
+++ app/models/bing.rb
@@ -1,3 +1,10 @@
 class Bing
 
+	def self.search(search_input)
+		bing_image = Bing.new('rNA3YpeZw7o2h8nOeRrZj9guzLdX1XTaXX7JYL9oypk', 25, 'Image')
+		search_string = search_input.split.join("+")  
+		result = bing_image.search("#{search_string}")
+		parsed_result = result[0][:Image]
+	end
+
 end
