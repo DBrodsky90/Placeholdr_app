f0f53aa506c9ec487421d03256d8922d819a9024
llevine <LaurenLevineDesigns@gmail.com>
2014-11-19 13:58:37 -0500

    updated the routes file, added commas to the seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root 'splash#index'
 
   resources :users
   resources :boards
   resources :images, except: [:edit, :update]
 
+
+  get 'sessions/new' => 'sessions#new', as: 'login'
+  post 'sessions'    => 'sessions#create'
+  delete 'sessions'  => 'sessions#destroy'
+  
 end
