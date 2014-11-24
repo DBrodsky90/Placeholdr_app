cc5d134561164ffeae28147bb2af465ba91d6571
llevine <LaurenLevineDesigns@gmail.com>
2014-11-24 10:24:51 -0500

    made the current user a universal method available within the views pages. Fixed the boards partials to .include?. Styled the boards edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+# 
+  helper_method :current_user
+
   def authenticate
     redirect_to login_path unless session[:current_user_id]
   end
