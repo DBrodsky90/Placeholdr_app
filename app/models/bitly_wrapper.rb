af4f67f928babc3a6c1100628afc01b60ab06b62
mandytrex <amandatrexler1012@gmail.com>
2014-11-24 16:00:23 -0500

    config file to hide apis

--- app/models/bitly_wrapper.rb
+++ app/models/bitly_wrapper.rb
@@ -1,7 +1,7 @@
 class BitlyWrapper
 
 	def self.short(url)
-		bitly = Bitly.new("amandaleet", "R_8f39e91fd31279f3dd6dcb8facfaee7a")
+		bitly = Bitly.new(ENV["BITLY_USERNAME"], ENV["BITLY_API_KEY"])
 		bitly_result = bitly.shorten(url)
 		shortened_url = bitly_result.short_url
 	end
